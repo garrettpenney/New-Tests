7462439561d7106d8431e3d5751156c0799fc738
garrettpenney <44512161+garrettpenney@users.noreply.github.com>
2018-10-26 15:52:49 -0600

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,30 @@
+//set count
+if(localStorage.getItem("count") == null) {
+	localStorage.setItem("count", 0)
+}
+
+//update count
+
+function update() {
+	
+	document.getElementById("count").innerHTML = localStorage.getItem("count");
+
+}
+
+update();
+
+//add one to count () {
+
+function plusOne() {
+	localStorage.setItem("count", (Number(localStorage.getItem("count")) + 1));
+	update();
+}
+
+//reset count
+
+function reset() {	
+	 if (confirm("Are you sure you are not going to eat more candy")) {
+	 	localStorage.setItem("count", 0);
+	 	update()
+	 }
+}
